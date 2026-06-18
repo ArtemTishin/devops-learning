f1f93f96562ac71b498f26a665a8f81106219b95
ArtemTishin <tema.arttem@yandex.ru>
2026-06-18 16:11:50 +0300

    Добавили файл math.sh с математикой

---
+++ math.sh
@@ -0,0 +1,14 @@
+echo "Введи а: "
+read a
+
+echo "Введи b: "
+read b
+
+summ=$((a+b))
+prz=$((a*b))
+del=$((a/b))
+ost=$((a%b))
+echo "Сумма равна $summ"
+echo "Произведение равно $prz"
+echo "Деление равно $del"
+echo "Остаток от деления равен $ost"
